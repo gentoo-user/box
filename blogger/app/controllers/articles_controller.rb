47cc9389f1e64093d332b5842173cce32a39be16
Tomasz Sikora <BM-2DAE3QFC59f9PNduxqtuWeTxm9NsAXirRP@bitmessage.ch>
2013-06-26 16:56:43 +0200

    + ‘blogger/.gitignore’ + ‘blogger/Gemfile’ + ‘blogger/Gemfile.lock’ + ‘blogger/README.rdoc’ + ‘blogger/Rakefile’ + ‘blogger/app/assets/images/rails.png’ + ‘blogger/app/assets/javascripts/application.js’ + ‘blogger/app/assets/javascripts/articles.js.coffee’ + ‘blogger/app/assets/javascripts/comments.js.coffee’ + ‘blogger/app/assets/stylesheets/application.css’ ...

---
+++ blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+
+def index
+  @articles = Article.all
+end
+
+
+def show
+@article = Article.find(params[:id])
+@comment = Comment.new
+@comment.article_id = @article.id
+
+end
+
+
+def new
+@article = Article.new
+
+
+end
+
+
+def create
+ @article = Article.new(params[:article])
+  @article.save
+  redirect_to article_path(@article)
+   flash.notice = "Article '#{@article.title}' Created!"
+
+end
+
+
+def destroy
+  @article = Article.destroy(params[:id])
+  @articles = Article.all
+  redirect_to :action => "index"
+  flash.notice = "Article '#{@article.title}' Destroyed!"
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+
+def update
+  @article = Article.find(params[:id])
+  @article.update_attributes(params[:article])
+
+  flash.notice = "Article '#{@article.title}' Updated!"
+
+  redirect_to article_path(@article)
+end
+
+
+end
